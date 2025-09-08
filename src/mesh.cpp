d75e59c26309fd86163e5d1c8afd94520eba45c3
Eira Lee <jacktlee24@outlook.com>
2025-09-09 00:18:56 +0100

    Halfway there

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1,3 +1,5 @@
+// MAKE A H FOR MAIN
+
 #include "mesh.h"
 #include <iostream>
 #include <fstream>
@@ -5,16 +7,13 @@
 #include <vector>
 #include <string>
 
-
-Mesh::Mesh(const std::string& modelPath, const std::string& texturePath) : modelPath(modelPath), texturePath(texturePath) {
+Mesh::Mesh(const std::string& modelPath, const std::string& texturePath, HelloTriangleApplication* app) : modelPath(modelPath), texturePath(texturePath), app(app) {
     loadModel();
-    loadTexture();
+    createVertexBuffer();
+    createIndexBuffer();
 }
 
 Mesh::~Mesh() {
-    if (pixels) {
-        stbi_image_free(pixels);
-    }
 }
 
 void Mesh::loadModel() {
@@ -40,6 +39,7 @@ void Mesh::loadModel() {
             std::istringstream iss(line.substr(line.find(" ") + 1));
             float x, y;
             iss >> x >> y;
+            y *= -1;
             texCoords.push_back({x, y});
         } else if (key == "vn") {
             std::istringstream iss(line.substr(line.find(" ") + 1));
@@ -74,6 +74,8 @@ void Mesh::loadModel() {
                 }
                 if (colorIndex >= 0 && colorIndex < colors.size()) {
                     vertex.color = colors[colorIndex];
+                } else {
+                    vertex.color = {1.0f, 0.4f, 0.7f};
                 }
                 
                 vertices.push_back(vertex);
@@ -86,6 +88,168 @@ void Mesh::loadModel() {
     Mesh::indices = indices;
 }
 
-void Mesh::loadTexture() {
-    // Figure put mesh loading tommorrow
+void Mesh::createVertexBuffer() {
+    VkDeviceSize bufferSize = sizeof(vertices[0]) * vertices.size();
+
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+    app->createBuffer(bufferSize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+    
+    void* data;
+    vkMapMemory(app->device, stagingBufferMemory, 0, bufferSize, 0, &data);
+    memcpy(data, vertices.data(), (size_t)bufferSize);
+    vkUnmapMemory(app->device, stagingBufferMemory);
+    
+    app->createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, vertexBuffer, vertexBufferMemory);
+    
+    app->copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+
+    vkDestroyBuffer(app->device, stagingBuffer, nullptr);
+    vkFreeMemory(app->device, stagingBufferMemory, nullptr);
+}
+
+void Mesh::createIndexBuffer() {
+    VkDeviceSize bufferSize = sizeof(indices[0]) * indices.size();
+
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+    app->createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+    
+    void* data;
+    vkMapMemory(app->device, stagingBufferMemory, 0, bufferSize, 0, &data);
+    memcpy(data, indices.data(), (size_t) bufferSize);
+    vkUnmapMemory(app->device, stagingBufferMemory);
+
+    app->createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
+    
+    app->copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+    vkDestroyBuffer(app->device, stagingBuffer, nullptr);
+    vkFreeMemory(app->device, stagingBufferMemory, nullptr);
+}
+
+void Mesh::createTextureImage() {
+    pixels = stbi_load(texturePath.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+
+    VkDeviceSize imageSize = texWidth * texHeight * 4;
+
+    if (!pixels) {
+        throw std::runtime_error("Failed to load Texture Image!");
+    }
+
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+    app->createBuffer(imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+    
+    void* data;
+    vkMapMemory(app->device, stagingBufferMemory, 0, imageSize, 0, &data);
+    memcpy(data, pixels, static_cast<size_t>(imageSize));
+    vkUnmapMemory(app->device, stagingBufferMemory);
+    stbi_image_free(pixels);
+    pixels = nullptr;
+
+    app->createImage(texWidth, texHeight, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, textureImage, textureImageMemory);
+
+    app->transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+    app->copyBufferToImage(stagingBuffer, textureImage, static_cast<uint32_t>(texWidth), static_cast<uint32_t>(texHeight));
+    app->transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+
+    vkDestroyBuffer(app->device, stagingBuffer, nullptr);
+    vkFreeMemory(app->device, stagingBufferMemory, nullptr);
+}
+
+void Mesh::createTextureImageView() {
+    textureImageView = app->createImageView(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_ASPECT_COLOR_BIT);
+}
+
+
+void Mesh::createTextureSampler() {
+    VkPhysicalDeviceProperties properties{};
+    vkGetPhysicalDeviceProperties(app->physicalDevice, &properties);
+
+    VkSamplerCreateInfo samplerInfo{};
+    samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+    samplerInfo.magFilter = VK_FILTER_LINEAR;
+    samplerInfo.minFilter = VK_FILTER_LINEAR;
+
+    samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+
+    samplerInfo.anisotropyEnable = VK_TRUE;
+    samplerInfo.maxAnisotropy = properties.limits.maxSamplerAnisotropy;
+
+    samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+    samplerInfo.unnormalizedCoordinates = VK_FALSE;
+
+    samplerInfo.compareEnable = VK_FALSE;
+    samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+
+    samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+    samplerInfo.mipLodBias = 0.0f;
+    samplerInfo.minLod = 0.0f;
+    samplerInfo.maxLod = 0.0f;
+
+    if (vkCreateSampler(app->device, &samplerInfo, nullptr, &textureSampler) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create Texture Sampler!");
+    }
+}
+
+void Mesh::createDescriptorSets(VkDescriptorPool descriptorPool, VkDescriptorSetLayout descriptorSetLayout, const std::vector<VkBuffer>& uniformBuffers, VkDeviceSize bufferSize) {
+    std::vector<VkDescriptorSetLayout> layouts(MAX_FRAMES_IN_FLIGHT, descriptorSetLayout);
+    VkDescriptorSetAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+    allocInfo.descriptorPool = descriptorPool;
+    allocInfo.descriptorSetCount = static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT);
+    allocInfo.pSetLayouts = layouts.data();
+
+    descriptorSets.resize(MAX_FRAMES_IN_FLIGHT);
+    if (vkAllocateDescriptorSets(app->device, &allocInfo, descriptorSets.data()) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to allocate Descriptor Sets for mesh!");
+    }
+
+    for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
+        VkDescriptorBufferInfo bufferInfo{};
+        bufferInfo.buffer = uniformBuffers[i];
+        bufferInfo.offset = 0;
+        bufferInfo.range = bufferSize;
+
+        VkDescriptorImageInfo imageInfo{};
+        imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+        imageInfo.imageView = textureImageView;
+        imageInfo.sampler = textureSampler;
+
+        std::array<VkWriteDescriptorSet, 2> descriptorWrites{};
+
+        descriptorWrites[0].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+        descriptorWrites[0].dstSet = descriptorSets[i];
+        descriptorWrites[0].dstBinding = 0;
+        descriptorWrites[0].dstArrayElement = 0;
+        descriptorWrites[0].descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+        descriptorWrites[0].descriptorCount = 1;
+        descriptorWrites[0].pBufferInfo = &bufferInfo;
+
+        descriptorWrites[1].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+        descriptorWrites[1].dstSet = descriptorSets[i];
+        descriptorWrites[1].dstBinding = 1;
+        descriptorWrites[1].dstArrayElement = 0;
+        descriptorWrites[1].descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+        descriptorWrites[1].descriptorCount = 1;
+        descriptorWrites[1].pImageInfo = &imageInfo;
+
+        vkUpdateDescriptorSets(app->device, static_cast<uint32_t>(descriptorWrites.size()), descriptorWrites.data(), 0, nullptr);
+    }
+}
+
+
+void Mesh::cleanup(VkDevice device) {
+    vkDestroyBuffer(device, indexBuffer, nullptr);
+    vkFreeMemory(device, indexBufferMemory, nullptr);
+    vkDestroyBuffer(device, vertexBuffer, nullptr);
+    vkFreeMemory(device, vertexBufferMemory, nullptr);
+
+    vkDestroySampler(device, textureSampler, nullptr);
+    vkDestroyImageView(device, textureImageView, nullptr);
+    vkDestroyImage(device, textureImage, nullptr);
+    vkFreeMemory(device, textureImageMemory, nullptr);
 }
--- src/mesh.h
+++ src/mesh.h
@@ -11,32 +11,52 @@
 #include <iostream>
 #include <filesystem>
 
+class HelloTriangleApplication;
+
 class Mesh {
 public:
-    Mesh(const std::string& modelPath, const std::string& texturePath);
+    Mesh(const std::string& modelPath, const std::string& texturePath, HelloTriangleApplication* app);
     ~Mesh();
+
+    void createTextureImage();
+    void createTextureImageView();
+    void createTextureSampler();
+    void createDescriptorSets(VkDescriptorPool descriptorPool, VkDescriptorSetLayout descriptorSetLayout, const std::vector<VkBuffer>& uniformBuffers, VkDeviceSize bufferSize);
 
     const std::vector<Vertex>& getVertices() const {return vertices;}
     const std::vector<uint16_t>& getIndices() const {return indices;}
+    VkBuffer getVertexBuffer() const {return vertexBuffer;}
+    VkBuffer getIndexBuffer() const {return indexBuffer;}
+    std::vector<VkDescriptorSet> getDescriptorSets() const {return descriptorSets;}
 
-    int getTexWidth() const  {return texWidth;}
-    int getTexHeight() const {return texHeight;}
-    int getTexChannels() const {return texChannels;}
-    stbi_uc* getPixels() const {return pixels;}
-
-    bool hasTexture() const {return !texturePath.empty() && pixels != nullptr;}
-
-private:
-    void loadModel();
-    void loadTexture();
-
-    std::vector<Vertex> vertices;
-    std::vector<uint16_t> indices;
+    void cleanup(VkDevice device);
 
     std::string modelPath;
     std::string texturePath;
+    std::vector<Vertex> vertices;
+    std::vector<uint16_t> indices;
+
+    VkBuffer vertexBuffer;
+    VkDeviceMemory vertexBufferMemory;
+    VkBuffer indexBuffer;
+    VkDeviceMemory indexBufferMemory;
+
+    VkImage textureImage;
+    VkDeviceMemory textureImageMemory;
+    VkImageView textureImageView;
+    VkSampler textureSampler;
+
+    std::vector<VkDescriptorSet> descriptorSets;
+
+private:
+    HelloTriangleApplication* app;
+    
     int texWidth, texHeight, texChannels;
     stbi_uc* pixels = nullptr;
+
+    void loadModel();
+    void createVertexBuffer();
+    void createIndexBuffer();
 };
 
 #endif
